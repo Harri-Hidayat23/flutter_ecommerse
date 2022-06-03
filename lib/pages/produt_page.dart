393766f2948a9f676b955ad17853e8714ea3f95f
Harri Hidayat <harrihidayat92@gmail.com>
2022-06-03 16:56:31 +0700

    update 3 june 2022

--- lib/pages/produt_page.dart
+++ lib/pages/produt_page.dart
@@ -8,24 +8,47 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
+  List images = [
+    'assets/images_Product.png',
+    'assets/images_Product.png',
+    'assets/images_Product.png'
+  ];
+
+  List familiarProduct = [
+    'assets/images_Product.png',
+    'assets/images_Product.png',
+    'assets/images_Product.png',
+    'assets/images_Product.png',
+    'assets/images_Product.png',
+    'assets/images_Product.png',
+    'assets/images_Product.png',
+    'assets/images_Product.png',
+    'assets/images_Product.png'
+  ];
+
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    List images = [
-      'assets/images_Product.png',
-      'assets/images_Product.png',
-      'assets/images_Product.png'
-    ];
-
-    int currentIndex = 0;
-
     Widget indicator(int index) {
       return Container(
-        margin: EdgeInsets.symmetric(horizontal: 2),
         width: currentIndex == index ? 16 : 4,
         height: 4,
+        margin: EdgeInsets.symmetric(horizontal: 2),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             color: currentIndex == index ? primaryColor : Color(0xffc4c4c4)),
+      );
+    }
+
+    Widget familiarProductCard(String imageUrl) {
+      return Container(
+        width: 54,
+        height: 54,
+        margin: EdgeInsets.only(right: 16),
+        decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage(imageUrl)),
+            borderRadius: BorderRadius.circular(6)),
       );
     }
 
@@ -65,21 +88,182 @@ class _ProductPageState extends State<ProductPage> {
                       currentIndex = index;
                     });
                   })),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: images.map((e) {
-              index++;
-              return indicator(index);
-            }).toList(),
+          SizedBox(
+            height: 20,
+          ),
+          CarouselIndicator(
+            count: images.length,
+            index: currentIndex,
+            activeColor: priceColor,
+            color: Color(0xffc4c4c4),
           )
         ],
+      );
+    }
+
+    Widget content() {
+      int index = -1;
+
+      return Container(
+        margin: EdgeInsets.only(top: 17),
+        width: double.infinity,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(24),
+            ),
+            color: bgColor1),
+        child: Column(
+          children: [
+            // ! Title
+            Container(
+              margin: EdgeInsets.only(
+                  top: defaultMargin,
+                  right: defaultMargin,
+                  left: defaultMargin),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Product 1',
+                          style: primaryTextStyle.copyWith(
+                              fontSize: 18, fontWeight: semiBold),
+                        ),
+                        Text(
+                          'Categories',
+                          style: secondaryTextStyle.copyWith(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/button_whistlist.png',
+                    width: 46,
+                  )
+                ],
+              ),
+            ),
+            // !Price
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(
+                  top: 20, left: defaultMargin, right: defaultMargin),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4), color: bgColor2),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Price start from',
+                    style: primaryTextStyle,
+                  ),
+                  Text(
+                    'Rp.500.000',
+                    style: priceTextStyle.copyWith(
+                        fontSize: 16, fontWeight: semiBold),
+                  )
+                ],
+              ),
+            ),
+            // !Desc
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(
+                  top: 30, left: defaultMargin, right: defaultMargin),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Description',
+                    style: primaryTextStyle.copyWith(fontWeight: medium),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam commodo tincidunt turpis.',
+                    style: subtitleTextStyle.copyWith(fontWeight: light),
+                    textAlign: TextAlign.justify,
+                  )
+                ],
+              ),
+            ),
+            // !familiar product
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(top: defaultMargin),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                    child: Text(
+                      'Familiar Products,',
+                      style: primaryTextStyle.copyWith(fontWeight: medium),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: familiarProduct.map(
+                        (image) {
+                          index++;
+                          return Container(
+                              margin: EdgeInsets.only(
+                                  left: index == 0 ? defaultMargin : 0),
+                              child: familiarProductCard(image));
+                        },
+                      ).toList(),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            // ! Buttons
+            Container(
+              margin: EdgeInsets.all(defaultMargin),
+              child: Row(
+                children: [
+                  Container(
+                      width: 54,
+                      height: 54,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('assets/button_chat.png')))),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Container(
+                      height: 54,
+                      child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              backgroundColor: primaryColor),
+                          child: Text('Add Product',
+                              style: primaryTextStyle.copyWith(
+                                  fontSize: 16, fontWeight: semiBold))),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       );
     }
 
     return Scaffold(
       backgroundColor: bgColor6,
       body: ListView(
-        children: [header()],
+        children: [header(), content()],
       ),
     );
   }
